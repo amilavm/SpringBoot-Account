077aad2551101465c08e93a72ba555702cf4c846
Amila <amilalunuwila@gmail.com>
2018-02-18 23:49:34 +0530

    added userservice

---
+++ src/main/java/com/amila/account/Service/UserService.java
@@ -0,0 +1,12 @@
+package com.amila.account.Service;
+
+import com.amila.account.Model.UserAccount;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+
+}
